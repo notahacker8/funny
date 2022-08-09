7fc3aa10e85b711f827e8d204214215a372a6498
King Doggo <77363964+notahacker8@users.noreply.github.com>
2022-08-09 09:09:34 -0700

    Create ishowspeed.js

---
+++ ishowspeed.js
@@ -0,0 +1,4 @@
+(function(){
+var is = document.getElementsByTagName("IMG")
+for (var i = 0 ; i < is.length ; i++) {is[i].src="https://c.tenor.com/rV8mpdXgZpAAAAAd/i-show-speed-speed.gif"; is[i].crossorigin="anonymous"}
+})();

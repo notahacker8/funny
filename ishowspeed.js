00079fa0c0cae91a5828a1e0aa8ba3ee6e322972
King Doggo <77363964+notahacker8@users.noreply.github.com>
2022-08-10 07:54:04 -0700

    Update ishowspeed.js

--- ishowspeed.js
+++ ishowspeed.js
@@ -1,19 +1,8 @@
 (function(){
-var is = document.getElementsByTagName("IMG")
-for (var i = 0 ; i < is.length ; i++) {is[i].src="https://c.tenor.com/rV8mpdXgZpAAAAAd/i-show-speed-speed.gif"; is[i].crossorigin="anonymous"}
-  const targetNode = document;
-const config = { childList: true, subtree: true };
-
-const callback = function(mutationsList, observer) {
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            // Elements have changed
-          var is = document.getElementsByTagName("IMG")
-for (var i = 0 ; i < is.length ; i++) {is[i].src="https://c.tenor.com/rV8mpdXgZpAAAAAd/i-show-speed-speed.gif"; is[i].crossorigin="anonymous"}
-        }
-    }
-};
-
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+var imgs = document.getElementsByTagName("IMG")
+for (var i = 0 ; i < imgs.length ; i++) 
+{
+  imgs[i].src="https://c.tenor.com/rV8mpdXgZpAAAAAd/i-show-speed-speed.gif"; 
+  imgs[i].crossorigin="anonymous"
+}
 })();
